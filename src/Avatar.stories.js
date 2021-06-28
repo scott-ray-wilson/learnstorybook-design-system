fe6f048608b2c5af21b72070b08b23fb03f3a8f8
Scott Wilson <scottraywilson@gmail.com>
2021-06-27 17:56:24 -0700

    build

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { Avatar } from './Avatar';
+import { Avatar } from "./Avatar";
 
 export default {
-  title: 'Design System/Avatar',
+  title: "Design System/Avatar",
   component: Avatar,
 };
 
 export const Standard = (args) => <Avatar {...args} />;
 
 Standard.args = {
-  size: 'large',
-  username: 'Tom Coleman',
-  src: 'https://avatars2.githubusercontent.com/u/132554',
+  size: "large",
+  username: "Tom Coleman",
+  src: "https://avatars2.githubusercontent.com/u/132554",
 };
 
 export const Sizes = (args) => (
@@ -20,15 +20,14 @@ export const Sizes = (args) => (
     <Avatar {...args} size="large" />
     <Avatar {...args} size="medium" />
     <Avatar {...args} size="small" />
-    <Avatar {...args} siz e="tiny" />
+    <Avatar {...args} size="tiny" />
   </div>
 );
 
 Sizes.args = {
-  username: 'Tom Coleman',
-  src: 'https://avatars2.githubusercontent.com/u/132554',
+  username: "Tom Coleman",
+  src: "https://avatars2.githubusercontent.com/u/132554",
 };
-
 
 export const Initials = (args) => (
   <div>
@@ -63,3 +62,14 @@ export const Large = (args) => (
     />
   </div>
 );
+
+// New story using controls
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+Controls.args = {
+  loading: false,
+  size: "tiny",
+  username: "Dominic Nguyen",
+  src: "https://avatars2.githubusercontent.com/u/263385",
+};
